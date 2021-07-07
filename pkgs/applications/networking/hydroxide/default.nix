d677cf495e2d8cfa646a17cef71885c907237ff4
Janus <janus@dark.fi>
2021-07-07 14:58:22 -0400

    Update hydroxide

--- pkgs/applications/networking/hydroxide/default.nix
+++ pkgs/applications/networking/hydroxide/default.nix
@@ -2,18 +2,24 @@
 
 buildGoModule rec {
   pname = "hydroxide";
-  version = "0.2.20";
+  version = "0.2.19";
 
   src = fetchFromGitHub {
-    owner = "emersion";
+    owner = "zkjanus";
     repo = pname;
     rev = "v${version}";
     sha256 = "sha256-VTUpiuSsI795XDSxJJvLQlVNPLiekHyKcCazRBky9nU=";
   };
 
-  vendorSha256 = "sha256-AuZnHpJ1Xel/L9dG3ATdXnoTeUxtieah/ea+0svw3oA=";
+  vendorSha256 = "sha256-g3p8EDsDk5niODYZv5QWVoOKnzmcWqARJjFgN9oqn/s=";
 
   doCheck = false;
+
+  runVend = false;
+
+  patches = [
+    ./utf8.patch
+  ];
 
   subPackages = [ "cmd/hydroxide" ];
 
